3e785c46c2f852953e6f2c9d1693bdf23ba796e0
Khalid War <git@khalidwar.com>
2020-12-03 14:33:20 -0600

    Removed unused packages
    Signed-off-by: Khalid War <git@khalidwar.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/all.dart';
 import 'package:yaanyo/screens/authentication/initial_screen.dart';
-import 'package:yaanyo/services/service_locator.dart';
 
 void main() async {
-  initServiceLocator();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(ProviderScope(child: MyApp()));
