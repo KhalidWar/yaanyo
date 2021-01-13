4a17109dcb588bd1f77c4c0c0ee9afab93519ca4
Khalid War <git@khalidwar.com>
2021-01-12 18:12:32 -0600

    Amending last commit
    Signed-off-by: Khalid War <git@khalidwar.com>

--- lib/state_management/settings_state_manager.dart
+++ lib/state_management/settings_state_manager.dart
@@ -1,9 +1,21 @@
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/all.dart';
+import 'package:yaanyo/services/user_database_service.dart';
 import 'package:yaanyo/state_management/providers.dart';
 
-class ConfirmationDialogs {
+final settingsManagerProvider =
+    ChangeNotifierProvider((ref) => SettingsStateManager());
+
+class SettingsStateManager extends ChangeNotifier {
+  void updateName(
+      BuildContext context, GlobalKey<FormState> formKey, String newName) {
+    if (formKey.currentState.validate()) {
+      context.read(userServiceProvider).updateUserName(newName);
+      Navigator.pop(context);
+    }
+  }
+
   void signOut(context) {
     showModal(
       context: context,
@@ -19,7 +31,9 @@ class ConfirmationDialogs {
                   Navigator.pop(context);
                 }),
             FlatButton(
-                child: Text('No'), onPressed: () => Navigator.pop(context))
+              child: Text('No'),
+              onPressed: () => Navigator.pop(context),
+            )
           ],
         );
       },
